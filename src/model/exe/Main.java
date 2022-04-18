7df94061d0e42cf4fa9b0e1d744a0a05daba40a7
Pedro Citadin Coelho <36762723+PedroCitadin@users.noreply.github.com>
2022-04-18 13:10:11 -0300

    Update Main.java

--- src/model/exe/Main.java
+++ src/model/exe/Main.java
@@ -20,21 +20,26 @@ public class Main {
         String nome;
          
         try{
+        //dialog para pegar o nome da busca    
         nome = JOptionPane.showInputDialog(null, "Digite o nome desejado");
         }catch(NullPointerException e){
             nome = "nulo";
         }
+        //// objeto criado para rodar o dialog do file chooser e consequentemente pegar o diretório desejado
         Main obj = new Main();
+        
         File f = obj.run(args);
-        
+        ///marcação de tempo no inicio da busca
         long inicio = System.currentTimeMillis();
-        
+        ///metodo para buscar o nome no diretório
         busc.pesquisaNome(nome, f);
+        
+        ///laço de repetição que verifica se todas as threads (com excessão da thread principal) ja completaram a execução
         while(Thread.activeCount()>1){
             
         }
         
-
+        //marcação do tempo final pós busca
         long fim = System.currentTimeMillis();
         long tempo = fim - inicio;
         System.out.println("Tempo decorrido: "+tempo+" milissegundos");
@@ -44,6 +49,8 @@ public class Main {
     
     
     public File run(String args[]){
+        
+        ////método apenas para rodar o File Chooser como dialog
         JFileChooser dc = new JFileChooser();
         dc.setMultiSelectionEnabled(false);
         dc.setCurrentDirectory(new File(""));
